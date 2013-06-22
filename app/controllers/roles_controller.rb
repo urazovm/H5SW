0d8ac6bd9e32cf525a2c4f1b334cc6669c9659ce
rails <rails@mylaptop.(none)>
2013-06-22 21:25:17 +0530

    Changed in jobs and notes

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -16,7 +16,6 @@ class RolesController < ApplicationController
 
   def create
     @role = current_company.roles.new(params[:role])
-    @roles = current_company.roles.all
     @role.save
     respond_to do |format|
       format.js
