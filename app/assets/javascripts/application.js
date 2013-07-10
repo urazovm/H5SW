6721b7459d469d8a91f2e5e372862d2a8f149012
rails <rails@mylaptop.(none)>
2013-07-10 17:38:29 +0530

    removed best in place gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require jquery.remotipart
 //= require twitter/bootstrap
-//= require best_in_place
 //= require jquery-ui
 //= require autocomplete-rails
 //= require_tree .
