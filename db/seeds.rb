0adfc274904e2bef50910ea0ad8aa993025da1b2
rails <rails@mylaptop.(none)>
2013-07-23 14:34:32 +0530

    modified seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 
 (1..10).each do |i|
-  Item.create(:itemtype => "SMO_item#{i}", :qty => i, :number => "SMOITEM#{i+1}", :name => "itemsmo#{i}", :description => "good", :unit_cost => i+20, :unit_price => i+10)
+  Item.create(:itemtype => "SMO_item#{i}", :qty => 1, :number => "SMOITEM#{i+1}", :name => "itemsmo#{i}", :description => "good", :unit_cost => i+20, :unit_price => i+10)
 end
 Item.create(:itemtype => "Service", :qty => 1, :number => "101", :name => "Regular Time", :description => "Service item for regular time", :unit_cost => 250, :unit_price => 200)
 Item.create(:itemtype => "Service", :qty => 1, :number => "102", :name => "Over Time", :description => "Service item for over time", :unit_cost => 350, :unit_price => 300)
