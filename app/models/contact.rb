f9827c320ac00b248efd9e32b55af884d47113a9
rails <rails@mylaptop>
2013-08-17 10:20:28 +0530

    no changes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -11,7 +11,7 @@ class Contact < ActiveRecord::Base
 
   def self.search(search)
     if search
-      where('name LIKE?', "%#{search}%")
+      where('name ILIKE?', "%#{search}%")
     else
       scoped
     end
